01fb243a3f8436c7d3af4005236300912fbc876c
Ian Holst <ianholst@gmail.com>
2018-04-02 18:06:31 -0400

    Added paper and code

---
+++ stuff.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class NFWHalo:
+
+    def __init__(self, M200, c):
+        self.M200 = M200
+        self.c = c
+
+    def averageConvergence(R):
+        pass
+
+    def shear():
+        pass
+
+class IsothermalHalo:
+
+    def __init__(self, sigma, rc):
+        self.sigma = sigma
+        self.rc = rc
+
+    def averageConvergence():
+        pass
+
+class BackgroundGalaxy:
+
+    def __init__(self, DS, DSL):
+        self.arg = arg
