d6c138f8254efd2a3cd5aabda9a2822edc7140c2
Ian Holst <ianholst@gmail.com>
2018-04-03 17:51:35 -0400

    collaborative coding !

--- lensing.py
+++ lensing.py
@@ -1,10 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
+import astropy.cosmology as cosmology
+from astropy.visualization import astropy_mpl_style
+import astropy.units as u
+import astropy.constants as const
 
-c = 1
-G = 1
-RHO_CRIT = 1
+plt.style.use(astropy_mpl_style)
+
+c = const.c
+G = const.G
+H0 = cosmology.default_cosmology.get().H(0)
+RHO_CRIT = 3 * H0**2 / (8 * np.pi * G)
+
 
 def SIGMA_CRIT(DS, DL):
     DSL = DS - DL
@@ -42,37 +50,67 @@ class IsothermalHalo:
         return 2 * reducedShear / (1 + reducedShear**2)
 
     def lense(self, galaxy):
-        R = np.sqrt(galaxy.x**2 + galaxy.y**2)
-        phi = np.degrees(np.arctan2(galaxy.y, galaxy.x))
-        epsilon = self.ellipticity(R, galaxy.DS)
+        beta = np.sqrt(galaxy.beta_x**2 + galaxy.beta_y**2)
+        phi = np.degrees(np.arctan2(galaxy.beta_y, galaxy.beta_x))
+        epsilon = self.ellipticity(self.DL*beta.to_value(u.rad), galaxy.DS)
         area_scaling = np.sqrt(1 / (1 - epsilon))
-        return Ellipse(xy=[galaxy.x, galaxy.y], width=(1-epsilon)*galaxy.size, height=galaxy.size, angle=phi)
+        # deflection angle: beta -> theta
+        return LensedBackgroundGalaxy(galaxy.beta_x, galaxy.beta_y, galaxy.a, galaxy.e1, galaxy.e2, galaxy.DS)
 
 
 class BackgroundGalaxy:
 
-    def __init__(self, e_intrinsic, size, x, y, DS):
+    def __init__(self, beta_x, beta_y, a, e1, e2, DS):
+        self.beta_x = beta_x
+        self.beta_y = beta_y
+        self.a = a
+        self.e1 = e1
+        self.e2 = e2
         self.DS = DS
-        self.e_intrinsic = e_intrinsic
-        self.size = size
-        self.x = x
-        self.y = y
-        self.ellipse = Ellipse(xy=[x,y], width=size, height=size, angle=0)
+
+    def ellipse(self):
+        a = 1
+        b = 1
+        phi = 1
+        x = (self.DS * self.beta_x.to_value(u.rad)).to_value(u.Mpc)
+        y = (self.DS * self.beta_y.to_value(u.rad)).to_value(u.Mpc)
+        return Ellipse(xy=[x,y], width=a, height=b, angle=phi)
+
+class LensedBackgroundGalaxy:
+
+    def __init__(self, theta_x, theta_y, a, e1, e2, DS):
+        self.theta_x = theta_x
+        self.theta_y = theta_y
+        self.a = a
+        self.e1 = e1
+        self.e2 = e2
+        self.DS = DS
+
+    def ellipse(self):
+        a = 1
+        b = 1
+        phi = 1
+        x = (self.DS * self.theta_x.to_value(u.rad)).to_value(u.Mpc)
+        y = (self.DS * self.theta_y.to_value(u.rad)).to_value(u.Mpc)
+        return Ellipse(xy=[x,y], width=a, height=b, angle=phi)
 
 
-
-halo = IsothermalHalo(1, 1, 1)
-backgroundGalaxies = [BackgroundGalaxy(1, 3, 100*np.random.rand()-50, 100*np.random.rand()-50, 10) for i in range(300)]
+halo = IsothermalHalo(sigma=1*u.m/u.s, rc=1*u.Mpc, DL=1*u.Gpc)
+backgroundGalaxies = [BackgroundGalaxy(
+    beta_x=(100*np.random.rand()-50)*u.arcsec,
+    beta_y=(100*np.random.rand()-50)*u.arcsec,
+    e1=0,
+    e2=0,
+    a=1*u.arcsec,
+    DS=10*u.Gpc) for i in range(300)]
 
 fig = plt.figure(dpi=150)
 ax = fig.add_subplot(111, aspect='equal')
 
 for gal in backgroundGalaxies:
-    galellipse = halo.lense(gal)
-    galellipse.set_facecolor(np.random.rand(3))
-    ax.add_artist(galellipse)
-
-ax.set_xlim(-50, 50)
-ax.set_ylim(-50, 50)
+    lensedGal = halo.lense(gal)
+    lensedGalEllipse = lensedGal.ellipse()
+    lensedGalEllipse.set_facecolor(np.random.rand(3))
+    ax.add_artist(lensedGalEllipse)
 
 plt.show()
