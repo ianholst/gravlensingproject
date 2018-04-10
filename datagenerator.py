7286b6831e425b68922a0d03a7cd21d7355d91e1
dbyun425 <doybyun@gmail.com>
2018-04-10 17:34:44 -0400

    added comments. functionally identical

--- datagenerator.py
+++ datagenerator.py
@@ -1,3 +1,13 @@
+"""
+Created by Doyee Byun
+April 10, 2018
+
+Goals:
+Figure out how to create data using code from lensing.py
+define initial conditions for creating data
+galaxy locations, lens profile, etc.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
@@ -6,10 +16,11 @@ from astropy.visualization import astropy_mpl_style
 import astropy.units as u
 import astropy.constants as const
 
+#import profiles from lensing.py
 from lensing import NFWHalo
 from lensing import IsothermalHalo
 
 # data format is theta_x, theta_y, e1, e2
 datafile = open('output.txt','w')
-datafile.write("Hello world")
+datafile.write("Hello world") #Dummy code for testing write function
 datafile.close()
