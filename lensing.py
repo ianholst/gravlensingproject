ef87c92bcb29d4521f7ef5762360f2b2faebc7bc
Ian Holst <ianholst@gmail.com>
2018-04-18 12:27:39 -0400

    lensing.py is now much more legitimate

--- lensing.py
+++ lensing.py
@@ -13,7 +13,7 @@ c = const.c
 G = const.G
 H0 = cosmology.default_cosmology.get().H(0)
 RHO_CRIT = 3 * H0**2 / (8 * np.pi * G)
-
+u.set_enabled_equivalencies(u.dimensionless_angles())
 
 def SIGMA_CRIT(DS, DL):
     DSL = DS - DL
@@ -22,94 +22,109 @@ def SIGMA_CRIT(DS, DL):
 
 class NFWHalo:
 
-    def __init__(self, M200, c_param, DL):
+    def __init__(self, M200, C, DL):
         self.M200 = M200
-        self.c = c_param     #changed variable name for clarification
-        self.delta_c = (200/3) * c_param**3 / (np.log(1+c_param) - c_param/(1+c_param))
-        self.rs = 1/c_param * ((3 * M200) / (800 * np.pi * RHO_CRIT))**(1/3)
+        self.C = C
+        self.delta_c = (200/3) * C**3 / (np.log(1+C) - C/(1+C))
+        r200 = ((3 * M200) / (800 * np.pi * RHO_CRIT))**(1/3)
+        self.rs = r200/C
         self.DL = DL
 
 
 class IsothermalHalo:
 
-    def __init__(self, sigma, rc, DL):
-        self.sigma = sigma
+    def __init__(self, M200, rc, DL):
+        self.M200 = M200
         self.rc = rc
         self.DL = DL
+        self.Tc = rc/DL * u.rad
+        r200 = ((3 * M200) / (800 * np.pi * RHO_CRIT))**(1/3)
+        self.sigmaSquared = M200 * G / (2 * (r200 - rc * np.arctan(r200/rc)))
 
-    def surfaceDensity(self, R):
-        return self.sigma**2 / (2 * G * np.sqrt(R**2 + self.rc**2))
+    def surfaceDensity(self, T):
+        # Surface density at angle T
+        return self.sigmaSquared / (2 * G * self.DL * np.sqrt(T**2 + self.Tc**2))
 
-    def averageSurfaceDensity(self, R):
-        return self.sigma**2 * (np.sqrt(R**2 + self.rc**2) - self.rc) / (G * R**2)
+    def averageSurfaceDensity(self, T):
+        # Average surface density at angle T
+        return self.sigmaSquared * (np.sqrt(T**2 + self.Tc**2) - self.Tc) / (G * self.DL * T**2)
 
-    def shear(self, R, DS):
-        return (self.averageSurfaceDensity(R) - self.surfaceDensity(R)) / SIGMA_CRIT(DS, self.DL)
+    def shear(self, T, DS):
+        # Tangential shear at angle T for object at distance DS
+        return (self.averageSurfaceDensity(T) - self.surfaceDensity(T)) / SIGMA_CRIT(DS, self.DL)
 
-    def ellipticity(self, R, DS):
-        reducedShear = self.shear(R, DS) / (1 - self.averageSurfaceDensity(R) / SIGMA_CRIT(DS, self.DL))
+    def ellipticity(self, T, DS):
+        # Magnitude of ellipticity at angle T for object at distance DS
+        convergence = self.surfaceDensity(T) / SIGMA_CRIT(DS, self.DL)
+        reducedShear = self.shear(T, DS) / (1 - convergence)
         return 2 * reducedShear / (1 + reducedShear**2)
 
-    def deflection(beta, ):
-        dd
+    def deflection(self, Bx, By, DS):
+        # Gives the image angle theta (T) that an object at source angle beta (B) and distance DS is deflected to
+        return Bx, By
 
     def lense(self, galaxy):
-        beta = np.sqrt(galaxy.beta_x**2 + galaxy.beta_y**2)
-        phi = np.degrees(np.arctan2(galaxy.beta_y, galaxy.beta_x))
-        epsilon = self.ellipticity(self.DL*beta.to_value(u.rad), galaxy.DS)
-        area_scaling = np.sqrt(1 / (1 - epsilon))
         # deflection angle: beta -> theta
-        return LensedBackgroundGalaxy(galaxy.beta_x, galaxy.beta_y, galaxy.a, galaxy.e1, galaxy.e2, galaxy.DS)
+        Tx, Ty = self.deflection(galaxy.Bx, galaxy.By, galaxy.DS)
+        T = np.sqrt(Tx**2 + Ty**2)
+        phi = np.arctan2(Ty, Tx)
+        e = self.ellipticity(T, galaxy.DS)
+        e1 = -e*np.cos(2*phi)
+        e2 = -e*np.sin(2*phi)
+        return LensedBackgroundGalaxy(Tx, Ty, galaxy.a, e1, e2, galaxy.DS)
 
 
 class BackgroundGalaxy:
 
-    def __init__(self, beta_x, beta_y, a, e1, e2, DS):
-        self.beta_x = beta_x
-        self.beta_y = beta_y
+    def __init__(self, Bx, By, a, e1, e2, DS):
+        self.Bx = Bx
+        self.By = By
         self.a = a
         self.e1 = e1
         self.e2 = e2
         self.DS = DS
 
-    def ellipse(self):
-        a = 1
-        b = 1
-        phi = 1
-        x = (self.DS * self.beta_x.to_value(u.rad)).to_value(u.Mpc)
-        y = (self.DS * self.beta_y.to_value(u.rad)).to_value(u.Mpc)
-        return Ellipse(xy=[x,y], width=a, height=b, angle=phi)
 
 class LensedBackgroundGalaxy:
 
-    def __init__(self, theta_x, theta_y, a, e1, e2, DS):
-        self.theta_x = theta_x
-        self.theta_y = theta_y
+    def __init__(self, Tx, Ty, a, e1, e2, DS):
+        self.Tx = Tx
+        self.Ty = Ty
         self.a = a
         self.e1 = e1
         self.e2 = e2
         self.DS = DS
+        self.T = np.sqrt(Tx**2 +Ty**2)
+        self.phi = np.arctan2(Ty, Tx)
 
     def ellipse(self):
-        a = .01
-        b = .01
-        phi = 1
-        x = (self.DS * self.theta_x.to_value(u.rad)).to_value(u.Mpc)
-        y = (self.DS * self.theta_y.to_value(u.rad)).to_value(u.Mpc)
-        return Ellipse(xy=[x,y], width=a, height=b, angle=phi)
+        e = np.sqrt(self.e1**2 + self.e2**2)
+        return Ellipse(xy=[self.Tx.to_value(u.arcsec),self.Ty.to_value(u.arcsec)],
+                       width=self.a,
+                       height=self.a*(e+1)/(e-1),
+                       angle=self.phi.to_value(u.degree))
 
-def main():
-    halo = IsothermalHalo(sigma=1*u.m/u.s, rc=1*u.Mpc, DL=1*u.Gpc)
+
+if __name__ == '__main__':
+    v = 100
+
+    halo = IsothermalHalo(
+        M200=1e15*u.solMass,
+        rc=10*u.kpc,
+        DL=1*u.Gpc)
+
     backgroundGalaxies = [BackgroundGalaxy(
-        beta_x=(100*np.random.rand()-50)*u.arcsec,
-        beta_y=(100*np.random.rand()-50)*u.arcsec,
+        Bx=(v*np.random.rand()-v/2)*u.arcsec,
+        By=(v*np.random.rand()-v/2)*u.arcsec,
         e1=0,
         e2=0,
-        a=1*u.arcsec,
-        DS=10*u.Gpc) for i in range(300)]
+        a=1,
+        DS=3*u.Gpc) for i in range(1000)]
 
-    fig = plt.figure(dpi=150)
+    fig = plt.figure(dpi=100)
     ax = fig.add_subplot(111, aspect="equal")
+    ax.set_xlim(-v/2,v/2)
+    ax.set_ylim(-v/2,v/2)
 
     for gal in backgroundGalaxies:
         lensedGal = halo.lense(gal)
@@ -118,3 +133,21 @@ def main():
         ax.add_artist(lensedGalEllipse)
 
     plt.show()
+
+    theta = np.linspace(0.0001,100,1000)*u.arcsec
+    epsilon = np.array([halo.ellipticity(t, 3*u.Gpc).to_value("") for t in theta])
+    gamma = np.array([halo.shear(t, 3*u.Gpc).to_value("") for t in theta])
+    kappa = np.array([(halo.surfaceDensity(t) / SIGMA_CRIT(3*u.Gpc, halo.DL)).to_value("") for t in theta])
+    mu = 1/((1-kappa)**2 - gamma**2)
+    plt.figure(dpi=100)
+    plt.plot(theta, epsilon)
+    plt.plot(theta, gamma)
+    plt.plot(theta, kappa)
+    plt.plot(theta, mu)
+    plt.legend(["$\epsilon$","$\gamma$","$\kappa$","$\mu$"])
+    plt.xlabel("$\\theta$")
+    plt.yscale("symlog", linthreshy=1)
+    plt.show()
+
+    (halo.rc/halo.DL).to(u.arcsec)
+    (4*np.pi*halo.sigmaSquared / c**2 * (3*u.Gpc - halo.DL)/(3*u.Gpc)).to(u.arcsec)
