Fixed issue with units and arrays

analysis.py
@@ -48,7 +48,6 @@ epsilon_mean, bin_edges, binnumber = binned_statistic(theta, epsilon, statistic=
 epsilon_sigma, bin_edges, binnumber = binned_statistic(theta, epsilon, statistic=np.std, bins=theta_bin_edges)
 theta_bin_centers = theta_bin_edges[:-1] + (theta_bin_edges[1] - theta_bin_edges[0])/2
 
-
 # FITTING
 def nfw_ellipticity(theta, M200, C):
     halo = NFWHalo(M200*u.solMass, C, DL)
@@ -62,7 +61,10 @@ BOUNDS = [[0,0], [np.inf, np.inf]]
 optimalNFWParams, covariance = curve_fit(nfw_ellipticity, theta_bin_centers, epsilon_mean, p0=[1e14, 10], bounds=BOUNDS, sigma=epsilon_sigma)
 optimalIsoParams, covariance = curve_fit(iso_ellipticity, theta_bin_centers, epsilon_mean, p0=[1e14, 10], bounds=BOUNDS, sigma=epsilon_sigma)
 
-plt.plot(theta_bin_centers, epsilon_mean)
+plt.figure()
 plt.plot(theta_bin_centers, nfw_ellipticity(theta_bin_centers, 1e15,10))
+plt.plot(theta_bin_centers, epsilon_mean)
 plt.plot(theta_bin_centers, nfw_ellipticity(theta_bin_centers, *optimalNFWParams))
 plt.plot(theta_bin_centers, iso_ellipticity(theta_bin_centers, *optimalIsoParams))
+plt.legend(["original", "binned data", "nfw fit", "isothermal fit"])
+plt.show()

lensing.py
@@ -29,7 +29,7 @@ class Halo:
 
     def shear(self, T, DS):
         # Tangential shear at angle T for object at distance DS
-        return (self.averageSurfaceDensity(T) - self.surfaceDensity(T)) / SIGMA_CRIT(DS, self.DL)
+        return ((self.averageSurfaceDensity(T) - self.surfaceDensity(T)) / SIGMA_CRIT(DS, self.DL)).to_value("")
 
     def ellipticity(self, T, DS):
         # Magnitude of ellipticity at angle T for object at distance DS
@@ -53,8 +53,8 @@ class Halo:
 
     def plot(self, start, stop, step, DS):
         theta = np.linspace(start, stop, step)*u.arcsec
-        epsilon = np.array([self.ellipticity(t, DS).to_value("") for t in theta])
-        gamma = np.array([self.shear(t, DS).to_value("") for t in theta])
+        epsilon = np.array([self.ellipticity(t, DS) for t in theta])
+        gamma = np.array([self.shear(t, DS) for t in theta])
         kappa = np.array([(self.surfaceDensity(t) / SIGMA_CRIT(DS, self.DL)).to_value("") for t in theta])
         mu = 1/((1-kappa)**2 - gamma**2)
         plt.figure()